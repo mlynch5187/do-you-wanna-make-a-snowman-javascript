4148f97592d970e4a31a484b45423a509902da2f
Michael Alex Lynch <mlynch5187@gmail.com>
2021-03-08 10:49:09 -0700

    Humans start with no materials

--- src/Human.js
+++ src/Human.js
@@ -2,6 +2,10 @@ class Human {
   constructor(name) {
     this.name = name;
     this.wantsToBuildASnowman = true;
+    this.materials = {snowballs: 0,
+                      coal: 0,
+                      buttons: 0,
+                      carrots: 0}
   }
 }
 
